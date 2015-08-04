58f511ef721472afc274d5dccab7785cc301f9d9
TimGoulain <timotheegoulain@hotmail.fr>
2015-08-04 22:05:18 +0000

    start modeling users

--- db/migrate/20141109150817_create_users.rb
+++ db/migrate/20141109150817_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.datetime :birth
+      t.string :job
 
       t.timestamps
     end
