ddbb27ca30bf08a1e32cb45ba48967b4ad191a2b
Tim <timotheegoulain@hotmail.fr>
2015-08-25 10:29:35 +0000

    Work on offer css

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -3,7 +3,7 @@ class Sector < ActiveRecord::Base
   
   # Associations
   has_many :jobs
-  has_attached_file :image, styles: { icon: "32x32" }
+  has_attached_file :image, styles: { icon: "32x32", small: "64x64", med: "100x100" }
   
     # Validations
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
