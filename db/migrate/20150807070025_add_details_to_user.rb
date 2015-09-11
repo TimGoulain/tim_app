ef158bff30d46525b69971f6b4328b4dfb9e52e6
Tim <timotheegoulain@hotmail.fr>
2015-09-11 14:18:38 +0000

    Remove job from users

--- db/migrate/20150807070025_add_details_to_user.rb
+++ db/migrate/20150807070025_add_details_to_user.rb
@@ -1,6 +1,5 @@
 class AddDetailsToUser < ActiveRecord::Migration
   def change
-    add_column :users, :job, :string
     add_column :users, :phone, :integer
     add_column :users, :competencies, :string
   end
